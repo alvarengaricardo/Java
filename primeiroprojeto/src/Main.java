6bfc1a054f28e9083d8cb040d51643daa94be301
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-02 03:04:49 -0300

    atribuindo valores

---
+++ primeiroprojeto/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Screen Match");
+        System.out.println("Filme: Top Gun Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        
+
+    }
+}
