0ebf410a55318ec5f730b3cccb66386b65a3c1e0
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-03 02:30:20 -0300

    Leitura de Dados

---
+++ primeiroprojeto/src/Leitura.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite o nome do filme:");
+        String filme = leitura.nextLine();
+
+        System.out.println("Qual o ano de lançamento?");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("Qual a sua avaliação (0 - 10)?");
+        Double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+    }
+}
