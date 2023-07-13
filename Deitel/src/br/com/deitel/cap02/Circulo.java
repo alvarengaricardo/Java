f7f216df5d8889b39715a9649cc33b7dcd6a7bfd
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-13 02:50:23 -0300

    capítulo 2 - 29

---
+++ Deitel/src/br/com/deitel/cap02/Circulo.java
@@ -0,0 +1,22 @@
+package br.com.deitel.cap02;
+
+import java.util.Scanner;
+
+/*
+ * Exercicio 28
+ * Calcular diâmetro, circunferência e área de um círculo
+ */
+public class Circulo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor do raio: ");
+        float raio = input.nextFloat();
+
+        System.out.println("Diâmetro: " + (2 * raio));
+        System.out.println("Circunferência: " + (2 * Math.PI * raio));
+        System.out.println("Área: " + (Math.PI * Math.pow(raio, 2)));
+
+    }
+}
---
+++ Deitel/src/br/com/deitel/cap02/Unicode.java
@@ -0,0 +1,22 @@
+package br.com.deitel.cap02;
+
+import java.util.Scanner;
+
+/*
+ * Exercicio 29
+ * Exibir o valor Unicode de um caracter
+ */
+public class Unicode {
+    public static void main(String[] args) {
+
+        System.out.println("O valor unicode de 'A' é: " + ((int) 'A'));
+        System.out.println("O valor unicode de 'a' é: " + ((int) 'a'));
+        System.out.println("O valor unicode de 'B' é: " + ((int) 'B'));
+        System.out.println("O valor unicode de 'b' é: " + ((int) 'b'));
+        System.out.println("O valor unicode de 'R' é: " + ((int) 'R'));
+        System.out.println("O valor unicode de 'r' é: " + ((int) 'r'));
+        System.out.println("O valor unicode de 'Z' é: " + ((int) 'Z'));
+        System.out.println("O valor unicode de 'z' é: " + ((int) 'z'));
+
+    }
+}
