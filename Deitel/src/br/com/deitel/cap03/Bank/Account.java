8e4b627f16affdf90265a000b33ff0673d8a0758
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-16 23:06:52 -0300

    bank 000

---
+++ Deitel/src/br/com/deitel/cap03/Bank/Account.java
@@ -0,0 +1,13 @@
+package br.com.deitel.cap03.Bank;
+
+public class Account {
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
