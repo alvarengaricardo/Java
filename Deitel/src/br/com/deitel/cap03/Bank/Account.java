566d85427aa37de174a6fc5ba41cae080dd9043c
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-20 14:14:32 -0300

    inclusão de saldo e depósito

--- Deitel/src/br/com/deitel/cap03/Bank/Account.java
+++ Deitel/src/br/com/deitel/cap03/Bank/Account.java
@@ -2,14 +2,18 @@ package br.com.deitel.cap03.Bank;
 
 public class Account {
     private String name;
+    private double balance;
 
-    public Account(String name){
+    public Account(String name, double balance){
         this.name = name;
-    }
 
+        if(balance >= 0) this.balance = balance;
+
+    }
+/*
     public Account(){
-
     }
+ */
 
     public void setName(String name){
         this.name = name;
@@ -18,4 +22,14 @@ public class Account {
     public String getName(){
         return name;
     }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void deposit (double depositAmount){
+        if (depositAmount > 0) balance = balance + depositAmount;
+    }
+
+
 }
--- Deitel/src/br/com/deitel/cap03/Bank/AccountTest.java
+++ Deitel/src/br/com/deitel/cap03/Bank/AccountTest.java
@@ -7,18 +7,25 @@ public class AccountTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Account account1 = new Account("Jane Green");
-        Account account2 = new Account("John Blue");
+        Account account1 = new Account("Jane Green", 50.00);
+        Account account2 = new Account("John Blue", -7.53);
 
         System.out.printf("Account1 name is: %s%n", account1.getName());
+        System.out.printf("Account1 balance is: %.2f%n", account1.getBalance());
         System.out.printf("Account2 name is: %s%n", account2.getName());
+        System.out.printf("Account2 balance is: %.2f%n", account2.getBalance());
 
+ /*
         Account account3 = new Account();
         System.out.print("Account3 name: ");
         String name = input.nextLine();
         account3.setName(name);
         System.out.printf("Account3 name is: %s%n", account3.getName());
+*/
 
-
+        System.out.print("Account1 deposit: ");
+        Double deposit = input.nextDouble();
+        account1.deposit(deposit);
+        System.out.printf("Account1 balance is: %.2f%n", account1.getBalance());
     }
 }
