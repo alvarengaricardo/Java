644174b1f8fb71120d83c1b95dd66895dc4d99db
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-25 17:23:43 -0300

    primeiros códigos com Javax

---
+++ Deitel/src/br/com/deitel/cap03/GUI/NameDialog.java
@@ -0,0 +1,11 @@
+package br.com.deitel.cap03.GUI;
+
+import javax.swing.*;
+
+public class NameDialog {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("What is your name?");
+        String message = String.format("Welcome, %s, to Java Programming!", name);
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
