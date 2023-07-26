62ce5960e876748ef2d46e2679eefbf4c5b49dfd
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-07-26 17:22:28 -0300

    área circulo

---
+++ onlineJudge/src/_01_beginner/_003_areaCirculo.java
@@ -0,0 +1,13 @@
+package _01_beginner;
+
+import java.util.Scanner;
+
+public class _003_areaCirculo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double n = 3.14159;
+        double raio = entrada.nextDouble();
+        double area = n * Math.pow(raio, 2);
+        System.out.printf("A=%.4f%n", area);
+    }
+}
