a8558e4a684b5169cec2a05ca6455d0705c1f179
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-08-15 08:15:00 -0300

    while 2

--- Deitel/src/br/com/deitel/cap05/WhileCounter.java
+++ Deitel/src/br/com/deitel/cap05/WhileCounter.java
@@ -6,9 +6,13 @@ Exemplo básico de while
 public class WhileCounter {
     public static void main(String[] args) {
         int counter = 1;
+        int i = 0;
         while (counter <= 10){
             System.out.printf("Counter = %d%n", counter);
             counter++;
         }
+
+        while(++i <=10) System.out.printf("%d ", i); // while com apenas uma linha
+
     }
 }
