cbfb422e030409635f7788ccd9d990bdb961b539
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-08-19 21:04:44 -0300

    GUI Shapes

---
+++ Deitel/src/br/com/deitel/cap05/GUI/Shapes.java
@@ -0,0 +1,28 @@
+package br.com.deitel.cap05.GUI;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Shapes extends JPanel{
+    private int choice; // escolha do usuário sobre qual forma usar
+    // contrutor configura a escolha do usuário
+    public Shapes(int userChoice){
+        choice = userChoice;
+    }
+
+    // desenha uma cascata de formas
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+
+        for(int i = 0; i < 15; i++){
+            switch (choice){
+                case 1: // retângulos
+                    graphics.drawRect(10 + i * 10, 10 + i * 10, 50 + i * 10, 50 + i * 10);
+                    break;
+                case 2: // ovais
+                    graphics.drawOval(10 + i * 10, 10 + i * 10, 50 + i * 10, 50 + i * 10);
+                    break;
+            }
+        }
+    }
+}
