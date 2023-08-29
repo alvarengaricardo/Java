2cca7229325c35169bf5687082ac1a652bb3bc29
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-08-29 17:43:51 -0300

    GUI imagens gráficas cores e formas

---
+++ Deitel/src/br/com/deitel/Cap06/GUI/DrawSmileyTest.java
@@ -0,0 +1,14 @@
+package br.com.deitel.Cap06.GUI;
+
+import javax.swing.JFrame;
+public class DrawSmileyTest {
+    public static void main(String[] args) {
+        DrawSmiley panel = new DrawSmiley();
+        JFrame application = new JFrame();
+
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(panel);
+        application.setSize(250, 270);
+        application.setVisible(true);
+    }
+}
