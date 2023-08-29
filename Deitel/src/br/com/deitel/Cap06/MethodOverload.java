31215bafa4181ccfe415d7ac311705e512155dea
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-08-29 16:57:02 -0300

    sobrecarga de método

---
+++ Deitel/src/br/com/deitel/Cap06/MethodOverload.java
@@ -0,0 +1,25 @@
+package br.com.deitel.Cap06;
+
+/*
+Declaração de métodos sobrecarregados
+ */
+public class MethodOverload {
+    public static void main(String[] args) {
+        // chamada de métodos sobrecarregados
+        System.out.printf("Square of integer 7 is %d%n", square(7));
+        System.out.printf("Square of double 7.5 is %f%n", square(7.5));
+
+    }
+
+    // método com int
+    public static int square(int intValue){
+        System.out.printf("%nCalled square with int argument: %d%n", intValue);
+        return intValue * intValue;
+    }
+
+    // método com double
+    public static double square(double doubleValue){
+        System.out.printf("%nCalled square with double argument: %f%n", doubleValue);
+        return doubleValue * doubleValue;
+    }
+}
