a00e7e522aa0aaaeda6cf09939c1aa8c8b483952
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-09-21 23:44:45 -0300

    SumArray

--- Deitel/src/br/com/deitel/cap07/SumArray.java
+++ Deitel/src/br/com/deitel/cap07/SumArray.java
@@ -9,9 +9,16 @@ public class SumArray {
         int[] array = {12, 15, 3, 85, 64, 32, 78, 11, 40, 88};
         int sum = 0;
 
-        for (int i : array) {
-            sum += i;
-        }
+        for (int i : array) sum += i;
+
         System.out.printf("Total: %d%n", sum);
+
+        double[] array2 = {12.75, 15.05, 3.12, 85.00, 64.71, 32.65, 78.19, 11.99, 40.03, 88.40};
+        double sum2 = 0;
+
+        for (double x : array2) sum2 += x;
+
+        System.out.printf("Total: %.2f%n", sum2);
+
     }
 }
