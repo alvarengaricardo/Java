70990e31774ccf85e99543181d06a9d5dd6caeaa
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-10-14 15:27:17 -0300

    Tabuada

--- Deitel/src/br/com/deitel/cap07/InitArray.java
+++ Deitel/src/br/com/deitel/cap07/InitArray.java
@@ -7,7 +7,7 @@ public class InitArray {
     public static void main(String[] args) {
 
         final int ARRAY_LENGTH = 10;
-        int[] array = new int[10];
+        int[] array1 = new int[10];
         int[] array2 = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
         int[] array3 = new int[ARRAY_LENGTH];
 
@@ -15,11 +15,11 @@ public class InitArray {
             array3[counter] = 2 + 2 * counter;
         }
 
-        System.out.printf("%s%9s%9s%9s%n", "Index", "Array", "Array2", "Array3");
+        System.out.printf("%s%9s%9s%9s%n", "Index", "Array1", "Array2", "Array3");
 
-        for (int counter = 0; counter < array.length; counter++) {
+        for (int counter = 0; counter < array1.length; counter++) {
             System.out.printf("%5d%9d%9d%9d%n",
-                    counter, array[counter], array2[counter], array3[counter]);
+                    counter, array1[counter], array2[counter], array3[counter]);
         }
     }
 }
